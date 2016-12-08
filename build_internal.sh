0abf4c75b7808fd9bd864e3e541ed5e47049ce03
Yifan Lu <yifanlu@users.noreply.github.com>
2016-12-08 09:00:37 -0800

    Limit build cores to 2
    That's how much we get from travis
    
    [skip ci]

--- build_internal.sh
+++ build_internal.sh
@@ -7,12 +7,12 @@ unset CC
 mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=toolchain-x86_64-w64-mingw32.cmake
-make -j4 tarball
+make -j2 tarball
       ;;
     *)
 mkdir build
 cd build
 cmake ..
-make -j4 tarball
+make -j2 tarball
       ;;
 esac
