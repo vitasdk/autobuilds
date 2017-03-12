eeed6c3129250388e10459f9fb88746c324abb70
devnoname120 <devnoname120+gh@gmail.com>
2017-03-12 01:34:52 +0100

    Switch to -j4

--- build_internal.sh
+++ build_internal.sh
@@ -7,12 +7,12 @@ unset CC
 mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=toolchain-x86_64-w64-mingw32.cmake
-make -j2 tarball
+make -j4 tarball
       ;;
     *)
 mkdir build
 cd build
 cmake ..
-make -j2 tarball
+make -j4 tarball
       ;;
 esac
